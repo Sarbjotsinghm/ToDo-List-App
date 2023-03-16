a58078592c36ac8520d42f724cce2b8e005a62bf
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-16 09:25:00 -0600

    imported from react.

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState, useRef, Button} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  View,
+} from 'react-native';
