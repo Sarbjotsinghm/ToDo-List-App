beaf5767179ad12ce2581dc5335fdad015f0965a
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-16 09:27:13 -0600

    copy and pasted view from previous exercise.

--- App.js
+++ App.js
@@ -8,3 +8,19 @@ import {
   TextInput,
   View,
 } from 'react-native';
+
+const App = () => {
+  const [todos, fixTodos] = React.useState([]);
+  let newText = '';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={todos}
+        renderItem={renderlist}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={Header}
+      />
+    </SafeAreaView>
+  );
+};
